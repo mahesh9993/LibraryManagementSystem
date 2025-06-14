4a44fce9761925eb61718977be81bdf59a57387d
Mahesh Chathuranga <mahesh.chathuranga9993@gmail.com>
2025-06-14 23:48:53 +0530

    error_fix

--- LibraryManagementDekstop/Screens/Home.xaml.cs
+++ LibraryManagementDekstop/Screens/Home.xaml.cs
@@ -1,8 +1,11 @@
 ﻿using LibraryManagementDekstop.Interfaces;
+using LibraryManagementDekstop.Models;
 using LibraryManagementDekstop.Screens;
 using LibraryManagementDekstop.Services;
 using LibraryManagementDesktop.Services;
 using System.Collections.Generic;
+using System.Net.Http;
+using System.Text.Json;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
@@ -17,17 +20,61 @@ namespace DesktopApplication.Screens
             LoadBooks();
         }
 
-        private void LoadBooks()
-        {
-            List<Book> books = new List<Book>
-            {
-                new Book { Id = 1, BookName = "Madol Duuwa", Author = "Martin Wickramasinghe", Quantity = 6, StatusColor = Colors.Red },
-                new Book { Id = 2, BookName = "Grade 10 Maths", Author = "Master Guide", Quantity = 5, StatusColor = Colors.LimeGreen },
-                new Book { Id = 3, BookName = "Tamil Language", Author = "Master Guide", Quantity = 2, StatusColor = Colors.LimeGreen }
-            };
+        //private void LoadBooks()
+        //{
+        //    List<Book> books = new List<Book>
+        //    {
+        //        new Book { Id = 1, BookName = "Madol Duuwa", Author = "Martin Wickramasinghe", Quantity = 6, StatusColor = Colors.Red },
+        //        new Book { Id = 2, BookName = "Grade 10 Maths", Author = "Master Guide", Quantity = 5, StatusColor = Colors.LimeGreen },
+        //        new Book { Id = 3, BookName = "Tamil Language", Author = "Master Guide", Quantity = 2, StatusColor = Colors.LimeGreen }
+        //    };
 
-            BookGrid.ItemsSource = books;
+        //    BookGrid.ItemsSource = books;
+        //}
+
+        private async void LoadBooks()
+        {
+            try
+            {
+                var handler = new HttpClientHandler
+                {
+                    ServerCertificateCustomValidationCallback = HttpClientHandler.DangerousAcceptAnyServerCertificateValidator
+                };
+
+                using var client = new HttpClient(handler);
+                client.BaseAddress = new Uri("https://localhost:7034/");
+
+                var response = await client.GetAsync("api/Book/GetAllBooks");
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var json = await response.Content.ReadAsStringAsync();
+
+                    var apiResponse = JsonSerializer.Deserialize<ApiResponse<List<Book>>>(json, new JsonSerializerOptions
+                    {
+                        PropertyNameCaseInsensitive = true
+                    });
+
+                    if (apiResponse != null && apiResponse.Data != null)
+                    {
+                        BookGrid.ItemsSource = apiResponse.Data;
+                    }
+                    else
+                    {
+                        MessageBox.Show("No books found.");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show($"Failed to load books: {response.StatusCode}");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error: {ex.Message}");
+            }
         }
+
 
         private void AddUser_Click(object sender, RoutedEventArgs e)
         {
@@ -85,14 +132,5 @@ namespace DesktopApplication.Screens
                 SearchBox.Foreground = Brushes.Gray;
             }
         }
-    }
-
-    public class Book
-    {
-        public int Id { get; set; }
-        public string BookName { get; set; }
-        public string Author { get; set; }
-        public int Quantity { get; set; }
-        public Color StatusColor { get; set; }
     }
 }
