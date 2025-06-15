4527d481cd9d233799711d81c3b4d0aa82934c5e
Nadil Malalanayake <150372922+NadilMalalanayake@users.noreply.github.com>
2025-06-15 17:59:39 +0530

    commit

---
+++ LibraryManagementAPI/Models/ReservationDetailInputModel.cs
@@ -0,0 +1,9 @@
+﻿namespace LibraryManagementAPI.Models
+{
+    public class ReservationDetailInputModel
+    {
+        public string? UserNumber { get; set; }
+        public string? BookNumber { get; set; }
+        public int CreatedBy { get; set; }
+    }
+}
---
+++ LibraryManagementDekstop/Models/ReserveDetailOutputModel.cs
@@ -0,0 +1,9 @@
+﻿namespace LibraryManagementDekstop.Models
+{
+    public class ReserveDetailOutputModel
+    {
+        public string UserNumber { get; set; }
+        public string BookNumber { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
